027029f91f62d85985e1ad1806535f6f3245b38e
Nel BOUVIER <48826816+nbouvier@users.noreply.github.com>
2022-11-29 16:26:46 +0100

    Verify test

--- packages/hardhat-configs/hardhat.config.ts
+++ packages/hardhat-configs/hardhat.config.ts
@@ -2,6 +2,8 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomiclabs/hardhat-ethers";
 import "@nomiclabs/hardhat-etherscan";
 import "./dist/index";
+import * as dotenv from "dotenv";
+dotenv.config();
 
 import "./tasks/calll-and-send";
 import "./tasks/deploy";
@@ -10,6 +12,18 @@ import "./tasks/get-network";
 
 const config: HardhatUserConfig = {
     solidity: "0.8.9",
+    networks: {
+        mumbai: {
+            url: "https://rpc.ankr.com/polygon_mumbai",
+            accounts: [ process.env.PRIVATE_KEY! ],
+            chainId: 80001
+        }
+    },
+    etherscan: {
+        apiKey: {
+            mumbai: process.env.MUMBAISCAN_API_KEY!
+        }
+    }
 };
 
 export default config;
